cdf477869235950aef39ea59ea76e73fcf38e0cc
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2020-07-12 19:48:04 -0300

    add entityCreated

--- src/commons/constants/index.ts
+++ src/commons/constants/index.ts
@@ -1,3 +1,4 @@
 export * from './headerConstants';
 export * from './errorConstants';
 export * from './httpConstants';
+export * from './models';
---
+++ src/commons/constants/models.ts
@@ -0,0 +1,3 @@
+interface EntityCreated {
+  id: number;
+}
