aa5a73181e2ac338277c12f117472da60d6b09fd
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2020-10-11 15:41:44 -0300

    add auth middleware

---
+++ src/packages/middlewares/auth.ts
@@ -0,0 +1,13 @@
+import { Request, Response, Router, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { HTTP400Error } from '../error';
+
+export const authMiddleware = (router: Router): void => {
+  router.use((req: Request, res: Response, next: NextFunction) => {
+    const token = <string>req.headers['access-token'];
+ 
+    if (!token) throw new HTTP400Error();
+    
+    const payload = jwt.verify(token, process.env.JWT_SIGNATURE || '');
+    next();
+})};
