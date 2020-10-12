21d816ecd1fb93e515d0c04d89ef9dba53df3ba9
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2020-10-11 21:25:43 -0300

    remove auth middleware

--- src/packages/middlewares/index.ts
+++ src/packages/middlewares/index.ts
@@ -2,4 +2,4 @@ import { authMiddleware } from './auth';
 import { handleBodyRequestParsing, handleCompression, checkHeaders } from './common';
 import { handleLogger } from './handleLogger';
 
-export default [checkHeaders, handleBodyRequestParsing, handleCompression, handleLogger,authMiddleware];
+export default [checkHeaders, handleBodyRequestParsing, handleCompression, handleLogger];
