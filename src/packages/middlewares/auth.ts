429d099a7a16e100f44e8d5b2084d69525793696
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2020-10-19 20:56:52 -0300

    add apply_auth

--- src/packages/middlewares/auth.ts
+++ src/packages/middlewares/auth.ts
@@ -1,10 +1,13 @@
 import { Request, Response, Router, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { HTTP400Error } from '../error';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const authMiddleware = (router: Router): void => {
   router.use((req: Request, res: Response, next: NextFunction) => {
-    if (req.path === process.env.READINESS || req.path === process.env.LIVENESS) return next();
+    if (req.path === process.env.READINESS || req.path === process.env.LIVENESS || process.env.APPLY_AUTH)
+      return next();
 
     const token = <string>req.headers['access-token'];
 
