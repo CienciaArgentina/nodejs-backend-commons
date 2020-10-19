bef5cf20bb7b13eccc27ef0f585d4ac7b2714d6c
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2020-10-19 20:09:19 -0300

    comment auth

--- src/packages/middlewares/auth.ts
+++ src/packages/middlewares/auth.ts
@@ -4,10 +4,11 @@ import { HTTP400Error } from '../error';
 
 export const authMiddleware = (router: Router): void => {
   router.use((req: Request, res: Response, next: NextFunction) => {
-    const token = <string>req.headers['access-token'];
- 
-    if (!token) throw new HTTP400Error();
-    
-    res.locals.jwt = jwt.verify(token, process.env.JWT_SIGNATURE || ''); 
+    // const token = <string>req.headers['access-token'];
+
+    // if (!token) throw new HTTP400Error();
+
+    // res.locals.jwt = jwt.verify(token, process.env.JWT_SIGNATURE || '');
     next();
-})};
+  });
+};
