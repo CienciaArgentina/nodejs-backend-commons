39b4a1f1d76de91513fa27686e44db5b9bce95a9
Lucas Francisco Lopez <lucaslopezf@gmail.com>
2020-10-21 01:49:10 -0300

    fix log

--- src/packages/logging/logger.ts
+++ src/packages/logging/logger.ts
@@ -1,5 +1,4 @@
-import winston, { createLogger, Logger } from 'winston';
-import { serviceName } from '../config';
+import winston, { createLogger, Logger, format } from 'winston';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -7,12 +6,15 @@ export const transports = {
   console: new winston.transports.Console(),
 };
 
+const readinessPath = process.env.READINESS || '/health';
+const livenessPath = process.env.LIVENESS || '/ping';
+const serviceName = process.env.SERVICE_NAME || '';
 const levelLogger = process.env.LOGGER_APP_LEVEL || 'info';
 
 export const logger: Logger = createLogger({
   level: levelLogger,
-  defaultMeta: { service: serviceName },
-  format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+  defaultMeta: { service: process.env.SERVICE_NAME },
+  format: format.combine(format.errors({ stack: true }), format.timestamp(), format.json()),
   transports: [transports.console],
 });
 
@@ -21,9 +23,9 @@ winston.exceptions.handle(transports.console);
 //Config log request and response
 const formatMessage = 'HTTP {{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}';
 export const configLoggerRequestResponse = {
-  ignoredRoutes: [<string>process.env.LIVENESS, <string>process.env.READINESS],
+  ignoredRoutes: [livenessPath, readinessPath],
   transports: [transports.console],
-  format: winston.format.combine(winston.format.timestamp(), winston.format.metadata(), winston.format.json()),
+  format: format.combine(format.timestamp(), format.metadata(), format.json()),
   meta: true,
   baseMeta: { service: serviceName },
   msg: formatMessage,
